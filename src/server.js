b5b148b7f5127436a7143493657d175fe953241f
Maja Hedegard <mh223pi@student.lnu.se>
2021-02-09 11:51:41 +0100

    set up express and logger

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
-console.log('hej')
+import express from 'express'
+import logger from 'morgan'
+
+const app = express()
+
+app.use(logger('dev'))
+
+app.get('/', (req, res) => res.send('Start page'))
+
+app.listen(8080, () =>
+  console.log('Server running now at http://localhost:8080/')
+)
