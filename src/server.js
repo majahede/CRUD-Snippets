21d64b0dfc250849481f2f52d4d6a48238fe360d
Maja Hedegard <mh223pi@student.lnu.se>
2021-02-09 17:07:57 +0100

    setup connection to database

--- src/server.js
+++ src/server.js
@@ -4,32 +4,46 @@ import logger from 'morgan'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 import { router } from './routes/router.js'
+import { connectDB } from './config/mongoose.js'
 
-const app = express()
+/**
+ * The main function of the application.
+ */
+const main = async () => {
+  try {
+    await connectDB()
+    const app = express()
 
-const directoryFullName = dirname(fileURLToPath(import.meta.url))
+    const directoryFullName = dirname(fileURLToPath(import.meta.url))
 
-// Set up morgan logger.
-app.use(logger('dev'))
+    // Set up morgan logger.
+    app.use(logger('dev'))
 
-// View enginge set up
-app.engine('hbs', hbs.express4({
-  defaultLayout: join(directoryFullName, 'views', 'layouts', 'default')
-}))
+    // View enginge set up
+    app.engine('hbs', hbs.express4({
+      defaultLayout: join(directoryFullName, 'views', 'layouts', 'default')
+    }))
 
-app.set('view engine', 'hbs')
-app.set('views', join(directoryFullName, 'views'))
+    app.set('view engine', 'hbs')
+    app.set('views', join(directoryFullName, 'views'))
 
-// Parse requests of the contetn type applications/x-www-form-urlencoded
-// Populates the request object with a body object (req.body)
-app.use(express.urlencoded({ extended: false }))
+    // Parse requests of the contetn type applications/x-www-form-urlencoded
+    // Populates the request object with a body object (req.body)
+    app.use(express.urlencoded({ extended: false }))
 
-// serve static files.
-app.use(express.static(join(directoryFullName, '..', 'public')))
+    // serve static files.
+    app.use(express.static(join(directoryFullName, '..', 'public')))
 
-// register routes
-app.use('/', router)
+    // register routes
+    app.use('/', router)
 
-app.listen(8080, () =>
-  console.log('Server running now at http://localhost:8080/')
-)
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running now at http://localhost:${process.env.PORT}`)
+    )
+  } catch (err) {
+    console.error(err.message)
+    process.exitCode = 1
+  }
+}
+
+main()
