a06c83a25dda37e07159d731993edd67d935907f
Maja Hedegard <mh223pi@student.lnu.se>
2021-02-15 10:13:24 +0100

    add bcrypt

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,26 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bycryptjs'
 
 // Create a schema.
 const userSchema = new mongoose.Schema({
   username: {
-    type: String
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
   },
   password: {
-    type: String
+    type: String,
+    minlength: [6, 'The password must be at least 6 charachters.'],
+    required: true
   }
+}, {
+  timestamps: true
+})
+
+// hash anbd salt
+userSchema.pre('save', async function () {
+  this.password = await bcrypt.hash(this.password, 8)
 })
 
 // create a model using the schema.
